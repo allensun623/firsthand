3071869d14307982202214185278790bb27a0bb8
Allen Sun <allensun623@gmail.com>
2024-03-04 16:46:21 -0800

    add inline-flex to align message

--- src/app/chatbot/ChatbotMessage.tsx
+++ src/app/chatbot/ChatbotMessage.tsx
@@ -7,50 +7,54 @@ import { messageTypeEnum } from '@/constants/messages';
 export default function ChatbotMessage({ text, type }: Message) {
   const messageBoxConfig = {
     [messageTypeEnum.CHATBOT]: {
+      justifyContent: 'flex-start',
       person: {
         textAlign: 'left',
         name: 'chatbot',
       },
       box: {
-        borderRadius: '5px 15px 15px 15px',
+        borderRadius: '5px 20px 20px 20px',
         background: (theme: any) => 'white',
       },
       textColor: 'black',
     },
     [messageTypeEnum.CUSTOMER]: {
+      justifyContent: 'flex-end',
       person: {
         textAlign: 'right',
         name: 'you',
       },
       box: {
-        borderRadius: '15px 5px 15px 15px',
+        borderRadius: '20px 5px 20px 20px',
         background: (theme: any) => theme.palette.primary.main,
       },
-      textColor: 'common.white',
+      textColor: 'white',
     },
   }[type];
 
   return (
     <Grid container direction='column'>
-      <Grid>
-        {
-          <Typography
-            color='common.grey'
-            variant='body2'
-            sx={{ textAlign: messageBoxConfig.person.textAlign }}
-          >
-            {messageBoxConfig.person.name}
-          </Typography>
-        }
+      <Grid
+        display='inline-flex'
+        justifyContent={messageBoxConfig.justifyContent}
+      >
+        <Typography color='grey' variant='body2'>
+          {messageBoxConfig.person.name}
+        </Typography>
       </Grid>
       <Grid
-        sx={(theme) => ({
-          borderRadius: messageBoxConfig.box.borderRadius,
-          background: messageBoxConfig.box.background(theme),
-        })}
-        p={2}
+        display='inline-flex'
+        justifyContent={messageBoxConfig.justifyContent}
       >
-        <Typography color={messageBoxConfig.textColor} variant='body2'>
+        <Typography
+          color={messageBoxConfig.textColor}
+          variant='body2'
+          sx={(theme) => ({
+            borderRadius: messageBoxConfig.box.borderRadius,
+            background: messageBoxConfig.box.background(theme),
+          })}
+          p={2}
+        >
           {text}
         </Typography>
       </Grid>
